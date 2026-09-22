066b2e5641f64d1239f5cc9560c655b579d501ab
Maher <abdulmateenmahe23@gmail.com>
2026-09-22 15:15:41 +0430

    Add GitHub Pages deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/typing-test-by-maher/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
